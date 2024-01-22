61f38251e1879e493fd77c9737a9393fe5f7eed9
franco-riss <143017101+franco-riss@users.noreply.github.com>
2024-01-21 22:39:34 -0300

    Fresh project with dependencies.

---
+++ src/main/java/com/damforum/DamForumApplication.java
@@ -0,0 +1,13 @@
+package com.damforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damforum/DamForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
